2bfae8622b31919b9fd8c43b4b394757c753e8c2
Alexandre Rademaker <arademaker@gmail.com>
2020-11-03 20:42:37 -0300

    material aula

--- Chapter53.hs
+++ Chapter53.hs
@@ -1,19 +1,21 @@
 module Chapter53 where
 
-data Tree a = Null | Node a (Tree a) (Tree a)
+data Tree a = Null | Node a (Tree a) (Tree a) deriving Show
+
+type Nat = Int
 
 merge:: Ord a => [a] -> [a] -> [a]
 merge [] ys  = ys
 merge xs []  = xs
 merge (x:xs) (y:ys)
-  | x <= y = x : merge xs (y:ys)
-  | otherwise = y : merge (x:xs) ys
+  | x <= y = x : merge xs (y : ys)
+  | otherwise = y : merge (x : xs) ys
 
-flatten::Ord a => Tree a -> [a]
-flatten Null = [ ]
+flatten :: Ord a => Tree a -> [a]
+flatten Null = []
 flatten (Node x u v) = x : merge (flatten u) (flatten v)
 
-heapify::Ord a => Tree a -> Tree a
+heapify :: Ord a => Tree a -> Tree a
 heapify Null         = Null
 heapify (Node x u v) = siftDown x (heapify u) (heapify v)
 
@@ -21,17 +23,35 @@ siftDown :: Ord a => a -> Tree a -> Tree a -> Tree a
 siftDown x Null Null = Node x Null Null
 
 siftDown x (Node y u v) Null
- |x <= y = Node x (Node y u v) Null
- | otherwise = Node y (siftDown x u v) Null
+  | x <= y = Node x (Node y u v) Null
+  | otherwise = Node y (siftDown x u v) Null
 
 siftDown x Null (Node y u v)
- |x <= y =Node x Null (Node y u v)
- | otherwise = Node y Null (siftDown x u v)
+  | x <= y = Node x Null (Node y u v)
+  | otherwise = Node y Null (siftDown x u v)
 
 siftDown x (Node y ul ur) (Node z vl vr)
- | x <= min y z = Node x (Node y ul ur) (Node z vl vr) 
- | y <= min x z = Node y (siftDown x ul ur) (Node z vl vr) 
- | z <= min x y = Node z (Node y ul ur) (siftDown x vl vr)
+  | x <= min y z = Node x (Node y ul ur) (Node z vl vr)
+  | y <= min x z = Node y (siftDown x ul ur) (Node z vl vr)
+  | z <= min x y = Node z (Node y ul ur) (siftDown x vl vr)
+
+
+-- mktree [] = Null
+-- mktree (x:xs) = Node x (mktree (take m xs)) (mktree (drop m xs))
+--   where
+--     m = length xs `div` 2
+
+
+-- mkpair n xs = (mktree (take n xs), drop n xs)
+mkpair :: Nat -> [a] -> (Tree a, [a])
+mkpair 0 xs = (Null, xs)
+mkpair n (x:xs) = (Node x l r, zs)
+  where (l, ys) = mkpair m xs
+        (r, zs) = mkpair (n - 1 - m) ys
+        m = (n - 1) `div` 2
+
+mktree xs = fst (mkpair (length xs) xs)    
+
 
 hsort :: Ord a => [a] -> [a]
 hsort = flatten . heapify . mktree
--- Chapter54.hs
+++ Chapter54.hs
@@ -1,33 +1,62 @@
 module Chapter54 where
 
 import qualified Chapter52 as M
+import Data.Array
 
-type LWord = [Char]
+-- https://stackoverflow.com/questions/31299146/name-conflict-in-haskell-while-creating-modules
+import Prelude hiding (Word)
 
-sortWords :: [LWord] -> [LWord]
+type Word = [Char]
+
+sortWords :: [Word] -> [Word]
 sortWords = M.msort
 
 -- >>> sortWords ["Alexandre","Roberto","Pedro","Jose","Jessica"]
 -- ["Alexandre","Jessica","Jose","Pedro","Roberto"]
 
+
 ordered :: Ord b => [a -> b] -> a -> a -> Bool
 ordered [] x y = True
 ordered (d:ds) x y = (d x < d y) || ((d x == d y) && ordered ds x y)
 
-data Tree a = Leaf a | Node [Tree a]
+data Tree a = Leaf a | Node [Tree a] deriving Show
 
-mktree :: (Bounded b, Enum b, Ord b) => [a -> b] -> [a] -> Tree [a]
-mktree [] xs = Leaf xs
-mktree (d:ds) xs = Node (map (mktree ds) (ptn d xs))
+-- mktree :: (Bounded b, Enum b, Ord b) => [a -> b] -> [a] -> Tree [a]
+-- mktree [] xs = Leaf xs
+-- mktree (d:ds) xs = Node (map (mktree ds) (ptn d xs))
 
-ptn :: (Bounded b, Enum b, Ord b) => (a -> b) -> [a] -> [[a]]
-ptn d xs = [filter (\x -> d x == m) xs | m <- rng]
- where rng = [minBound .. maxBound]
+-- ptn :: (Bounded b, Enum b, Ord b) => (a -> b) -> [a] -> [[a]]
+-- ptn d xs = [filter (\x -> d x == m) xs | m <- rng]
+--   where
+--     rng = [minBound .. maxBound]
+
+-- ptn :: Ix b => (b,b) -> (a -> b) -> [a] -> [[a]]
+-- ptn (l, u) d xs = elems xa
+--   where
+--     xa = accumArray snoc [] (l, u) (zip (map d xs) xs)
+--     snoc xs x = xs ++ [x]
+
+ptn (l, u) d xs = map reverse (elems xa)
+  where
+    xa = accumArray (flip (:)) [] (l, u) (zip (map d xs) xs)
+
 
 flatten :: Tree [a] -> [a]
 flatten (Leaf xs) = xs
 flatten (Node ts) = concatMap flatten ts
 
-bsort :: (Bounded b, Enum b, Ord b) => [a -> b] -> [a] -> [a]
-bsort ds xs = flatten (mktree ds xs)
+-- bsort :: (Bounded b, Enum b, Ord b) => [a -> b] -> [a] -> [a]
+
+-- bsort ds xs = flatten (mktree ds xs)
+bsort bb [] xs = xs
+-- bsort (d:ds) xs = concatMap (bsort ds) (ptn d xs)
+bsort bb (d:ds) xs = concatMap (bsort bb ds) (ptn bb d xs)
+
+-- rsort :: (Bounded b, Enum b, Ord b) => [a -> b] -> [a] -> [a]
+-- rsort [] xs = xs
+-- rsort (d:ds) xs = concat (ptn d (rsort ds xs))
+
+rsort :: Ix b => (b, b) -> [a -> b] -> [a] -> [a]
+rsort bb [] xs = xs
+rsort bb (d:ds) xs = concat (ptn bb d (rsort bb ds xs))
 
